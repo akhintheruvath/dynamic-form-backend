2984d0d0a0f77b2f76de72af40340f7d32b0ae04
Akhin <akhintheruvath2827@gmail.com>
2024-06-11 13:12:01 +0530

    Enabling CORS

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const logger = require('morgan');
+const cors = require('cors');
 const formConfigRoutes = require("./routes/formConfigRoutes");
 const app = express();
 const PORT = 8000;
 
 app.use(logger('dev'));
+app.use(cors());
 app.use("/api", formConfigRoutes);
 
 app.listen(PORT, () => {
